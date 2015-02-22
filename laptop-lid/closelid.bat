36e0d7bd0a716792898b871027794d564dd2b758
Rakhesh Sasidharan <me@rakhesh.com>
2015-02-22 20:34:27 +0400

    added modifications to the laptop lid closing script

--- laptop-lid/closelid.bat
+++ laptop-lid/closelid.bat
@@ -1,26 +1,25 @@
 @ECHO OFF
+setlocal enableextensions enabledelayedexpansion
 
-ping -n 1 10.134.202.21
-IF ERRORLEVEL 1 GOTO NOTLAN
+FOR /F "delims=," %%i in (C:\NRPortbl\officepoints.txt) do (
+   SET bHOSTUP=0
+   ping -n 1 %%i | find "TTL=" > NUL && SET bHOSTUP=1
+   IF !bHOSTUP! equ 1 GOTO INOFFICE
+)
 
-msg * "Machine is in office. Closing lid will not put laptop to sleep". 
-powercfg -setacvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
-powercfg -setdcvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0 
-GOTO EXIT
+GOTO OUTOFFICE
 
-:NOTLAN
-ping -n 1 192.168.0.1
-IF ERRORLEVEL 1 GOTO NOTWIFIMUS
+:INOFFICE
+msg * /time:2 "Machine is in office. Closing lid will not put laptop to sleep". 
+REM powercfg -setacvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+REM powercfg -setdcvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0 
+GOTO EOF
 
-msg * "Machine is in office. Closing lid will not put laptop to sleep". 
-powercfg -setacvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
-powercfg -setdcvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0 
-GOTO EXIT
+:OUTOFFICE
+msg /time:2 * "Machine is not in office. Closing lid will put laptop to sleep". 
+REM powercfg -setacvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1
+REM powercfg -setdcvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1
+GOTO EOF
 
-:NOTWIFIMUS
-msg * "Machine is not in office. Closing lid will put laptop to sleep". 
-powercfg -setacvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1
-powercfg -setdcvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1
-GOTO EXIT
-
-:EXIT
+:EOF
+exit /B
