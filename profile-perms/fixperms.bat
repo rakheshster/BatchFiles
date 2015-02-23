d4c5d40f4f3a49e089bf5a6c76d0a7820674e15c
Rakhesh Sasidharan <me@rakhesh.com>
2015-02-23 12:27:41 +0400

    fixperms changes before lunch

--- profile-perms/fixperms.bat
+++ profile-perms/fixperms.bat
@@ -1,7 +1,7 @@
+@ECHO OFF
 REM Run this in a folder full of roaming profiles (of format username.v2)
 REM This script will process each folder and if the context under which it is running is unable to view ACLs of the folder, it will add the BUILTIN\Administrators group to the ACL
 
-@ECHO OFF
 setlocal EnableDelayedExpansion
 set DOMAIN=%USERDOMAIN%
 
@@ -11,7 +11,7 @@ FOR /d %%D in ("*.v2") DO (
   echo Processing %%D
   
   REM Find the user to whom the profile belongs (so we can set ownership back once done)
-  FOR /F "delims=." %%U in ('echo %%D') DO echo User is %%U
+  FOR /F "delims=." %%U in ('echo %%D') DO set USER=%%U
   
   REM Try to find permissions; if ERRORLEVEL 0 then all is good; 5 implies Access Denied
   icacls %%D /Q 1> nul 2> nul
@@ -30,7 +30,7 @@ FOR /d %%D in ("*.v2") DO (
     echo    Enabling inheritance on sub-folders of %%D
     icacls %%D\* /inheritance:e /T 1> nul 2> nul
     
-    echo    Setting owner back to %%U
-    icacls %%D /setowner %DOMAIN%\%%U
+    echo    Setting owner back to %DOMAIN%\!USER!
+    icacls %%D /setowner %DOMAIN%\%USER%
   )
 )
