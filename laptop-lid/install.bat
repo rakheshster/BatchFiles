6577b70101b0192ff2b2e09b208366dfb8e502b3
Rakhesh Sasidharan <me@rakhesh.com>
2015-03-01 15:05:53 +0400

    Modified install.bat with correect paths.

--- laptop-lid/install.bat
+++ laptop-lid/install.bat
@@ -1,10 +1,16 @@
-copy /Y "\\vs-mussdata01\LocalData\Rakhesh\Scripts\Laptop-Lid\closelid.bat" C:\NRPortbl
-copy /Y "\\vs-mussdata01\LocalData\Rakhesh\Scripts\Laptop-Lid\officepoints.txt" C:\NRPortbl
+REM Batch file I use at work to copy the required files over to a user machine & create a scheduled task that runs as the user when they login
 
-powershell -Command "(gc \\vs-mussdata01\LocalData\Rakhesh\Scripts\Laptop-Lid\schtask.xml) -replace '--REPLACE--', $("$env:USERDOMAIN" + '\' + "$env:USERNAME") | Out-File -Encoding ASCII C:\NRPortbl\schtask.xml"
+REM Copy to C:\
+copy /Y "\\path\to\closelid.bat" C:\
+copy /Y "\\path\to\officepoints.txt" C:\
 
-schtasks /Create /XML "C:\NRPortbl\schtask.xml" /TN "Laptop Lid closing"
-del /f C:\NRPortbl\schtask.xml
+REM Copy the scheduled task file too, replacing as required
+powershell -Command "(gc \\path\to\schtask.xml) -replace '--REPLACE--', $("$env:USERDOMAIN" + '\' + "$env:USERNAME") | Out-File -Encoding ASCII C:\schtask.xml"
 
+REM Create the scheduled task & delete the file once done
+schtasks /Create /XML "C:\schtask.xml" /TN "Laptop Lid closing"
+del /f C:\schtask.xml
+
+REM Run the scheduled task once
 schtasks /Run /TN "Laptop Lid closing"
 pause
